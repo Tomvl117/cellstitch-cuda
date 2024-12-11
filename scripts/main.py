e3829b6295ac3012dd920ac9943b34e800372118
Tom van Leeuwen <tom@tleeuwen.nl>
2024-12-11 23:36:16 +0100

    Start conversion main pipeline to cupy

--- scripts/main.py
+++ scripts/main.py
@@ -1,17 +1,19 @@
 import torch
 import tifffile
 import os
+import cupy as cp
 from instanseg import InstanSeg
 from cellpose.utils import stitch3D
 from cellstitch.pipeline import full_stitch
 from cellstitch import preprocessing as pp
+from cellstitch import preprocessing_cupy as ppc
 
 
 stitch_method = "cellstitch"  # "iou" or "cellstitch"
-file_path = r"E:\1_DATA\Rheenen\tvl_jr\3d stitching\unmixed\unmixed.tif"
+file_path = r"E:\1_DATA\Rheenen\tvl_jr\3d stitching\unmixed2\unmixed.tif"
 out_path = os.path.split(file_path)[0]
 
-mode = "nuclei"  # Segmentation mode: "nuclei" or "cells" or "nuclei_cells"
+mode = "nuclei_cells"  # Segmentation mode: "nuclei" or "cells" or "nuclei_cells"
 
 # Read image file
 with tifffile.TiffFile(file_path) as tif:
@@ -25,17 +27,17 @@ z_resolution = 3.5
 
 model = InstanSeg("fluorescence_nuclei_and_cells")
 
-img = pp.histogram_correct(img)
+img = ppc.histogram_correct(cp.asarray(img))
 
 # Segment over Z-axis
-transposed_img = img.transpose(1, 2, 3, 0)  # ZCYX -> CYXZ
-yx_masks = pp.segmentation(transposed_img, model, pixel_size, mode).transpose(2, 0, 1)  # YXZ -> ZYX
+img = img.transpose(1, 2, 3, 0)  # ZCYX -> CYXZ
+yx_masks = ppc.segmentation(img, model, pixel_size, mode).transpose(2, 0, 1)  # YXZ -> ZYX
 tifffile.imwrite(os.path.join(out_path, "yx_masks.tif"), yx_masks)
 
 if stitch_method == "iou":
 
     # Memory cleanup
-    del model, img, transposed_img
+    del model, img
     if torch.cuda.is_available():
         torch.cuda.empty_cache()  # Clear GPU cache
 
@@ -47,14 +49,14 @@ if stitch_method == "iou":
 elif stitch_method == "cellstitch":
 
     # Segment over X-axis
-    transposed_img = img.transpose(1, 2, 0, 3)  # ZCYX -> CYZX
+    transposed_img = img.transpose(0, 1, 3, 2)  # CYXZ -> CYZX
     transposed_img, padding = pp.upscale_pad_img(transposed_img, pixel_size, z_resolution)  # Preprocess YZ planes
     yz_masks = pp.segmentation(transposed_img, model, pixel_size, mode)
     yz_masks = pp.crop_downscale_mask(yz_masks, padding, pixel_size, z_resolution).transpose(1, 0, 2)  # YZX -> ZYX
     tifffile.imwrite(os.path.join(out_path, "yz_masks.tif"), yz_masks)
 
     # Segment over Y-axis
-    transposed_img = img.transpose(1, 3, 0, 2)  # ZCYX -> CXZY
+    transposed_img = img.transpose(0, 2, 3, 1)  # CYXZ -> CXZY
     transposed_img, padding = pp.upscale_pad_img(transposed_img, pixel_size, z_resolution)  # Preprocess XZ planes
     xz_masks = pp.segmentation(transposed_img, model, pixel_size, mode)
     xz_masks = pp.crop_downscale_mask(xz_masks, padding, pixel_size, z_resolution).transpose(1, 2, 0)  # XZY -> ZYX
